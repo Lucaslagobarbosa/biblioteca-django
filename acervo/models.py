62b99d56fcef2937e4c0bc51a941b7f101a39e6e
Lucas Lago Barbosa <lucaslagobarbosa220@gmail.com>
2026-09-16 22:06:24 -0300

    fix: remove import duplicado e gera migracao inicial do model Livro

--- acervo/models.py
+++ acervo/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 class Livro(models.Model):
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=100)
