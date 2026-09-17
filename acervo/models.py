5549d159c2d143cfc584f463223a84bec6e0de7d
Lucas Lago Barbosa <lucaslagobarbosa220@gmail.com>
2026-09-16 22:35:52 -0300

    feat: adiciona campo tipo_acervo (Digital/Fisico) ao model Livro

--- acervo/models.py
+++ acervo/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 class Livro(models.Model):
+
+    TIPO_ACERVO_CHOICES = [
+        ('digital', 'Digital'),
+        ('fisico', 'Físico'),
+    ]
+
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=100)
     ano = models.IntegerField()
     disponivel = models.BooleanField(default=True)
+    tipo_acervo = models.CharField(
+        max_length=10,
+        choices=TIPO_ACERVO_CHOICES,
+        default='fisico'
+    )
 
     def __str__(self):
         return self.titulo
